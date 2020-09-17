77b9d77bddbb54ae3b1bf295227899128edfc802
xterminate <ilkin5@mail.ru>
2020-09-17 17:04:10 +0300

    Исправляет отображение голосов/группировка голосов

--- Classes/BackendAjaxControllerProvider.php
+++ Classes/BackendAjaxControllerProvider.php
@@ -414,9 +414,15 @@ class BackendAjaxControllerProvider implements ControllerProviderInterface
             $db = $app['db'];
 
             $qb = $db->createQueryBuilder();
-            $res = $qb->select('s.id', 's.name', 's.surname', 's.music_name', 'v.vote_count')
+            $res = $qb->select('s.id', 's.name', 's.surname', 's.music_name', 'count(v.vote_count) as vote_count')
                 ->from('`option`', 's')
                 ->leftJoin('s', 'vote', 'v', 's.id = v.poll_option_id')
+                ->groupBy([
+                    's.id',
+                    's.name',
+                    's.surname',
+                    's.music_name'
+                ])
                 ->execute();
 
             while ($row = $res->fetch()) {
